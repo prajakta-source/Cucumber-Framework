2e6ede1d04b2cf134a471fa0652868b1436ff732
sonali-patil02 <88033402+sonali-patil02@users.noreply.github.com>
2024-03-05 17:20:32 +0530

    Add files via upload

---
+++ testgit.java
@@ -0,0 +1,10 @@
+
+public class testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello git test project ");
+System.out.println("Update in main");
+	}
+
+}
